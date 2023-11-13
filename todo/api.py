fbbf6949506ae428aef4ca7f0372fedb4f8641c5
jod431047 <jod431047@gmail.com>
2023-11-13 22:25:28 +0100

    custum filter

--- todo/api.py
+++ todo/api.py
@@ -5,6 +5,7 @@ import django_filters.rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework import filters
+from.filters import TodoFilter
 
 class TodoViewsets(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
@@ -12,13 +13,15 @@ class TodoViewsets(viewsets.ModelViewSet):
     
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['description', 'name','status']
+    # filterset_fields = ['description', 'name','status']
     
     
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['description', 'name','status']
+    # filter_backends = [filters.SearchFilter]
+    # search_fields = ['description', 'name','status']
     
     
-    filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['id']
+    # filter_backends = [filters.OrderingFilter]
+    # ordering_fields = ['id']
+    
+    filterset_class =  TodoFilter
 
--- todo/filters.py
+++ todo/filters.py
@@ -2,9 +2,12 @@ from django_filters import rest_framework as filters
 from .models import Todo
 
 
-class ProductFilter(filters.FilterSet):
+class TodoFilter(filters.FilterSet):
     class Meta:
         model = Todo
         fields ={
             'status': ['exact'],
+            'id' : ['range'],
+            'name' : ['contains'],
+            'description' : ['contains'],
         }
